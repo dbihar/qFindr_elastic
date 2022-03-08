fc0e776330b36d93fff48d2af9a37801166b709a
Daniel Bihar <bihar.daniel@gmail.com>
2022-03-08 08:26:01 +0100

    plugin

--- openApp.js
+++ openApp.js
@@ -1,19 +1,24 @@
 const spawn = require('child_process').spawn
 const os = require('os')
 const path = require('path')
+const nodeCmd = require('node-cmd');
+const exec = require('child_process').exec;
 
 module.exports = (pluginContext) => {
   return (app, env = {}) => {
     return new Promise((resolve, reject) => {
       if (process.platform === 'win32' || process.platform === 'darwin') {
-        //console.info("Run this:")
-        //console.info(app)
-        const exec = require('child_process').exec;
+        console.info("Run this:")
+        if(!app.includes("start")){app = "\"".concat(app).concat("\"")}
+        console.info(app)
+        
         const proccessing = exec(app);
         proccessing.stdout.on('data', function(data) {
           console.log(data); 
         });
-        return resolve("")
+        console.log(consoproccessing.exitCode)
+        //nodeCmd.get(app, (err, data, stderr) => console.log(data));
+        return resolve(app)
       }
 
       // Use absolute paths at all time
